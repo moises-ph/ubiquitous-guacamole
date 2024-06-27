3f8a2afd33a1690eb6915b8a91d9036878435f74
ethiem guerrero <ethiemalex03@gmail.com>
2024-06-27 14:17:45 -0500

    some websockets finally

--- chat-server/main.go
+++ chat-server/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 	"github.com/moises-ph/ubiquitous-guacamole/chat-server/db"
 	"github.com/moises-ph/ubiquitous-guacamole/chat-server/handlers"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
 	err := godotenv.Load()
@@ -20,6 +27,18 @@ func main() {
 	//Users routes
 	router.POST("/user", handlers.UserRegister)
 	router.GET("/user", handlers.GetUser)
+	router.GET("/ws", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			c.JSON(400, err.Error())
+			return
+		}
+		defer conn.Close()
+		for {
+			conn.WriteMessage(websocket.TextMessage, []byte("Hello, WebSocket!"))
+			time.Sleep(time.Second)
+		}
+	})
 
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
